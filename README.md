819c6721cf56380dfce96b584b57aa4d718a0242
karaxuna <kaxabernet@gmail.com>
2015-12-18 01:29:09 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+## Universal Windows Platform (uwp) samples using JXcore
+
+With the help of uwp api you can use windows specific methods such as detecting devices connected or reading files from special folders... JXcore allows to do it with JavaScript. It supports multiple JavaScript engines. If you build JXcore with chakra engine, then you will get uwp support. To build JXcore chakra engine you will need Visual studio 2015 installed on Windows 10.
+
+### Build
+
+Clone JXcore code from repository:
+
+```bash
+git clone https://github.com/jxcore/jxcore.git
+```
+
+Build with chakra engine:
+
+```bash
+vcbuild.bat --engine-chakra
+```
+
+After successfull build, you will have jxcore executable available in `Release/jx.exe`.
+To run samples, cd to samples directory and run:
+
+```bash
+<path-to-jxcore>/Release/jx.exe index.js
+```
+
+### uwp api call examples
+
+#### Battery status:
+
+```javascript
+if (Windows.System.Power.PowerManager.batteryStatus === batteryStatusEnum.notPresent) {
+	console.log('The battery is not present.');
+}
+```
+
+Available battery statuses are: `notPresent`, `discharging`, 'idle' and `charging`.
+You can also get percentage of charged battery and remaning time (See `index.js` for full samples).
+
+#### Reading/writing file in known windows folders, like pictures, documents, videos etc.
+
+```javascript
+Windows.Storage.KnownFolders.picturesLibrary.createFileAsync('sample.png',
+	Windows.Storage.CreationCollisionOption.replaceExisting).done(function () {
+		// created succesfully
+	}, function (err) {
+		// handle error
+	});
+```
+
+### Check if mouse is present:
+
+```javascript
+var mouseCapabilities = new Windows.Devices.Input.MouseCapabilities();
+if (!mouseCapabilities.mousePresent) {
+	console.log('Mouse is present');
+} else {
+	console.log('Mouse is not present');
+}
+```
+
+You can find more information about windows api [here](https://msdn.microsoft.com/en-us/library/windows/apps/br211377.aspx).
---
+++ index.js
@@ -0,0 +1,73 @@
+var uwp = jxcore.uwp;
+uwp.projectNamespace("Windows");
+
+// check battery status
+var batteryStatus = Windows.System.Power.PowerManager.batteryStatus;
+var batteryStatusEnum = Windows.System.Power.BatteryStatus;
+
+if (batteryStatus === batteryStatusEnum.notPresent) {
+	console.log('The battery or battery controller is not present.');
+} else if (batteryStatus === batteryStatusEnum.discharging) {
+	console.log('The battery is discharging.');
+} else if (batteryStatus === batteryStatusEnum.idle) {
+	console.log('The battery is idle.');
+} else if (batteryStatus === batteryStatusEnum.charging) {
+	console.log('The battery is charging.');
+}
+
+// check remaining charge percent
+var remainingChargePercent = Windows.System.Power.PowerManager.remainingChargePercent;
+console.log(remainingChargePercent + '% of battery is charged.');
+
+// check remaning battery time
+if (batteryStatus === batteryStatusEnum.discharging) {
+	var remainingDischargeTime = Windows.System.Power.PowerManager.remainingDischargeTime;
+	var remainingMinutes = parseInt(remainingDischargeTime / 60000);
+	console.log(remainingMinutes + ' minutes of battery time left.');
+} else {
+	console.log('Battery is not discharging. No meaning to calculate battery time.');
+}
+
+// mouse presence
+var mouseCapabilities = new Windows.Devices.Input.MouseCapabilities();
+if (!mouseCapabilities.mousePresent) {
+	console.log('Mouse is present');
+} else {
+	console.log('Mouse is not present');
+}
+
+// keyboard presence
+var keyboardCapabilities = new Windows.Devices.Input.KeyboardCapabilities();
+if (!mouseCapabilities.keyboardPresent) {
+	console.log('Keyboard is present');
+} else {
+	console.log('Keyboard is not present');
+}
+
+// file operations
+var sampleFileName = 'sample.png',
+	picturesLibrary = Windows.Storage.KnownFolders.picturesLibrary,
+	replaceExisting = Windows.Storage.CreationCollisionOption.replaceExisting;
+
+(function createFileInPicturesFolder() {
+	picturesLibrary.createFileAsync(sampleFileName, replaceExisting).done(function (file) {
+	    console.log('File \'sample.png\' created in pictures folder');
+	    getFileFromPicturesFolder();
+	}, function (error) {
+	    console.log('Creating file failed:', error);
+	});
+})()
+
+function getFileFromPicturesFolder() {
+	console.log('Reading file that we just created...');
+	picturesLibrary.tryGetItemAsync(sampleFileName).done(function (file) {
+	    if (file !== null) {
+	        console.log('File name: ' + file.name);
+	    } else {
+	        console.log('Could not read file');
+	    }
+
+	    // close uwp
+	    uwp.close();
+	});
+}
